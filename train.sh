7e1bb71da2e587ce0d5d8700c2696f26716432e2
XU Yeqiao <xuyeqiao0529@gmail.com>
2024-03-16 12:30:28 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 train.py --trial_name toolsPercpective --annotation_folder /mnt/ceph/tco/TCO-Students/Projects/KP_curriculum_learning/cholec80/tool_annotations \
---data_folder /mnt/ceph/tco/TCO-Students/Projects/KP_curriculum_learning/cholec80/frames_1fps \
---output_folder /mnt/ceph/tco/TCO-Students/Projects/KP_curriculum_learning/cholec80/results/Approach6/Stage_6_Epoch_140_saveInWholeStage/
+python3 train.py --trial_name toolsPercpective --annotation_folder ...\
+--data_folder ... \
+--output_folder .../
